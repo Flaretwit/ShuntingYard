c956ee9bfd5f9f40e72d0eaa4742e5d20ab72475
ke shen <ikevinshen2@gmail.com>
2017-03-07 13:01:29 -0800

    Finished?

--- LinkedList.h
+++ LinkedList.h
@@ -1,54 +1,57 @@
 //LinkedList.h
-template <type T>
-#include <iostream>
-#include <vector>
-
+template <typename T>
 struct LLNode {
   LLNode<T>* next;
   T* data;
-}
+};
 
-template <type T>
-
+template <typename T>
 class LinkedList {
 public:
   //adds at a specific index
-  void addAt(LLNode<T>*);
+  void addAt(int index, LLNode<T>*, LLNode<T>*);
+  LinkedList();
   //note this adds to the end.
   void add(LLNode<T>*);
   LLNode<T>* getHead();
   LLNode<T>* deleteNode(int index);
-  void setNext(LLNode<T>*);
-  LLNode<T>* getNext();
 private:
   LLNode<T>* head;
+};
+
+//Has to be the same file because templates
+template <typename T>
+LinkedList<T>::LinkedList() {
+  LLNode<T>* head = NULL;
 }
 
-//Has to be the same file because templates?
-LinkedList::LinkedList() {
-  Node* head = new Node();
-}
+
 //adds at a specific index recursively
-void LinkedList::addAt(int index, LLNode<T>* node) {
+template <typename T>
+void LinkedList<T>::addAt(int index, LLNode<T>* node, LLNode<T>* head) {
   //the case where index to be added
   if(index == 0) {
-      node->setNext(head);
+      node->next = head;
       head = node;
   }
   else {
     index--;
-    head = head->getNext();
-    addAt(index, node, head);
+    head = head->next;
+    addAt(index, node, getHead());
   }
 }
+
 //returns the head of the LinkedList
-LLNode<T>* LinkedList::getHead() {
+template <typename T>
+LLNode<T>* LinkedList<T>::getHead() {
   return head;
 }
+
 //deletes a node given the index of the Node.
 //NEEDS ERROR CHECKING
-LLNode<T>* LinkedList::deleteNode(int index) {
-  Node* current = head;
+template <typename T>
+LLNode<T>* LinkedList<T>::deleteNode(int index) {
+  LLNode<T>* current = head;
   if(index == 0) {
     head = head->next;
     return current;
@@ -57,7 +60,7 @@ LLNode<T>* LinkedList::deleteNode(int index) {
     current = current->next;
     index--;
   }
-  Node* toReturn = current->next;
+  LLNode<T>* toReturn = current->next;
   current->next = current->next->next;
   return toReturn;
 }
--- main.cpp
+++ main.cpp
@@ -1,27 +1,104 @@
 //Shunting Yard Algorithm. Uses LinkedLists to implement a stack, which then acts
 //as a infix translator between prefix, infix and postfix.
 //main.cpp
-#include "LinkedList.h"
 #include <iostream>
 #include <vector>
+#include "LinkedList.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-LLNode<T>* pop(LinkedList*& ll);
-void push(LLNode<T>*);
-
+LLNode<char>* pop(LinkedList<char>*&);
+void push(LinkedList<char> *&, LLNode<char>*);
+LLNode<char>* peek(LinkedList<char> *);
+LLNode<char>* makeNode(char );
 using namespace std;
 
 int main() {
-  LinkedList*s ll = new LinkedList();
+  LinkedList<char>* ll = new LinkedList<char>;
   char* input = new char[80];
+  vector<char> output;
   cout << "Shunting Yard Algorithm" << endl;
+  cout << "Enter the mathematical expression." << endl;
+  cin.getline(input, 80);
 
+  bool done = false;
+
+
+  while(!done) {
+    char* token = new char[80];
+    int i;
+    for(int i = 0; *input != ' ' && *input != '\0'; i++, input++) {
+      token[i] = *input;
+    }
+    if(*input == '\0') {
+      while(peek(ll)->data != 0) {
+        output.push_back(*(pop(ll)->data));
+      }
+    }
+    if(isdigit(token[0])) {
+      output.push_back(token[0]);
+    }
+    else {
+      //if first token is a left parentheses.
+      if(token[0] == '(') {
+          push(ll, makeNode(token[0]));
+      }
+      //if first token is a right parentheses
+      else if(token[0] == ')') {
+        while(*(peek(ll)->data) != '(') {
+            output.push_back(*(pop(ll)->data));
+        }
+        //removes the left parentheses.
+        pop(ll);
+      }
+      //if stack is empty or contains a '(', push operator.
+      else if((token[0] == '+' || token[0] == '-' ||
+            token[0] == '*' || token[0] == '/' || token[0] == '^')
+            && (ll->getHead() == NULL || *(peek(ll)->data) == '(')) {
+         push(ll, makeNode(token[0]));
+      }
+       //pops from stack until operator is of higher precedence, then pushes
+       //the new operator onto the stack.
+       else {
+         bool finished = false;
+         while(!finished) {
+           if(token[0] == '^' && *(peek(ll)->data) != '^') {
+               finished = true;
+               push(ll, makeNode(token[0]));
+           }
+           else if((token[0] == '*' || token[0] == '/')
+           && (*(peek(ll)->data) == '+' || *(peek(ll)->data) == '-')) {
+             finished = true;
+             push(ll, makeNode(token[0]));
+           }
+           else {
+             output.push_back(*pop(ll)->data);
+           }
+         }
+       }
+    }
+
+  }
   return 0;
 }
-//returns the most recent node in the stack.
-LLNode<T>* pop(LinkedList*& ll) {
-  return ll->deleteNode(0);
+//does the operation when the character is a digit
+
+//gets the information for a node
+LLNode<char>* makeNode(char token) {
+  LLNode<char>* node = new LLNode<char>();
+  node->data = &token;
+  return node;
+}
+LLNode<char>* peek(LinkedList<char>* ll) {
+  return ll->getHead();
 }
 
-void push(LinkedList*& ll, LLNode<T>* node) {
-  ll->addAt(0, node);
+//returns the most recent node in the stack.
+LLNode<char>* pop(LinkedList<char>*& ll) {
+  return ll->deleteNode(0);
+}
+//adds a node to the top of the stack (implemented as a Linkedlist)
+void push(LinkedList<char>*& ll, LLNode<char>* node) {
+  ll->addAt(0, node, ll->getHead());
 }
